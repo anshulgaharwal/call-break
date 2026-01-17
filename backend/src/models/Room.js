176d0c64c4d959224868ec897d24c19feb45ceef
Devesh Yadav <thisisdkyadav@gmail.com>
2026-01-17 12:24:24 +0530

    feat: Add RoomMongoose model

--- backend/src/models/Room.js
+++ backend/src/models/Room.js
@@ -1,1 +1,15 @@
 // TODO: Room model/schema
+import mongoose from 'mongoose';
+
+const roomSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    password: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+export default Room;
