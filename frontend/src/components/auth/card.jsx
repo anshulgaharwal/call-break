484489df84e3817076cd7c2349efcfd0fd4d57b9
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-17 16:19:02 +0530

    login bg

--- frontend/src/components/auth/card.jsx
+++ frontend/src/components/auth/card.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+//src/components/auth/Crad.jsx
+import React from "react";
+import '../../styles/components/Card.css'
 
-const card = () => {
-  return (
-    <div>card</div>
-  )
-}
+const Card = () => {
+  return <div className="card-container">
+    <p>Card</p>
+  </div>;
+};
 
-export default card
+export default Card;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,20 @@
+//src/pages/Login.jsx
 import React from 'react'
+import '../styles/pages/Login.css'
+import Card from '../components/auth/card'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container'>
+        <video
+        className='bgVid'
+        src="/bg_vid.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline/>
+        <Card />
+    </div>
   )
 }
 
