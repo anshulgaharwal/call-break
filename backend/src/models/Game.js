3963f9e39cb855e14e8dac5b69cc40a1ee5005d6
Devesh Yadav <thisisdkyadav@gmail.com>
2026-01-17 12:47:27 +0530

    feat: Add Game Mongoose model with schema for game state, users, admin, and room.

--- backend/src/models/Game.js
+++ backend/src/models/Game.js
@@ -1,1 +1,28 @@
 // TODO: Game model/schema
+import mongoose from 'mongoose';
+
+const gameSchema = new mongoose.Schema({
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+    state: {
+        /**
+         * cards state in the game - object with key 0,1,2,3,4
+         * 0 for cards in center
+         * 1 for cards in player 1
+         * 2 for cards in player 2
+         * 3 for cards in player 3
+         * 4 for cards in player 4
+         * 
+         */
+        cards: { type: Object, default: {} }, 
+        currentPlayer: { type: Number, default: 0 },
+        
+    },
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+export default Game;
