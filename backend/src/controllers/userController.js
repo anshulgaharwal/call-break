3338db052e3803a10f356111457bfa7fd0b1c5c6
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-18 13:03:48 +0530

    added signin fnc

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -40,4 +40,35 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message: "User not find, try Sign up",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Wrong password, try again",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Sign in failed" });
+  }
+};
