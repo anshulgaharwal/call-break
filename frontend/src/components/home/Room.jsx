72bfdf7e8ecefcf448a28a3c49e53edf15aeb043
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-19 14:49:58 +0530

    added stucture for room ei create and join

--- frontend/src/components/home/Room.jsx
+++ frontend/src/components/home/Room.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Input from "../room/input";
+import { Divide } from "lucide-react";
+import Button from "../common/Button";
 
 const Room = ({ type }) => {
-    return (
-        <div>Room {type}</div>
-    )
-}
+  return (
+    <div>
+      <div className="header-room">
+        <h2>{type === "create" ? "Create Room" : "Join Room"}</h2>
+      </div>
+      {type === "create" && (
+        <div className="create-room">
+          <Input placeholder="Set room password (optional)" />
+          <Button>Create Room</Button>
+        </div>
+      )}
+      {type === "join" && (
+        <div className="join-room">
+          <Input placeholder="Enter Room ID" />
+          <Button>Join Room</Button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Room
+export default Room;
