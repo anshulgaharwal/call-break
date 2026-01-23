ec5378516da8f17909e99d7892628e396e6619ad
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-23 12:54:08 +0530

    added route for room details

--- backend/src/routes/roomRoutes.js
+++ backend/src/routes/roomRoutes.js
@@ -1,6 +1,6 @@
 // TODO: Room routes (create room, join room, etc.)
 import express from 'express';
-import { create, deleteRoom, join } from '../controllers/roomController.js';
+import { create, deleteRoom, getRoomDetails, join } from '../controllers/roomController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/create', authMiddleware, create);
 router.post('/join', authMiddleware, join);
 router.post('/delete', authMiddleware, deleteRoom);
+router.get("/:roomId", getRoomDetails);
 
 export default router;
